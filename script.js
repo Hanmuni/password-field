98978e23902f872b03bb79f9ff969a375ca938e5
Hồng-Hạnh, Chu <67581292+Hanmuni@users.noreply.github.com>
2023-11-06 22:31:10 +0100

    feat: toggle password in multiple forms

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-let checkbox = document.querySelector("#checkbox");
+const checkbox = document.querySelector("#checkbox");
 let password = document.querySelector("#password");
 
-checkbox.addEventListener("click", function () {
+checkbox.addEventListener("click", () => {
   if (checkbox.checked) {
     password.type = "text";
   } else {
     password.type = "password";
+  }
+});
+
+const checkbox2 = document.querySelector("#checkbox2");
+let passwords = document.querySelectorAll("#new-password, #current-password");
+
+checkbox2.addEventListener("click", () => {
+  for (let password of passwords) {
+    if (checkbox2.checked) {
+      password.type = "text";
+    } else {
+      password.type = "password";
+    }
   }
 });
